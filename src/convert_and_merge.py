94a8ba31b68bc13bd10d5e9ea0a201e372b8561f
Morgan <103436411+MorganKryze@users.noreply.github.com>
2023-12-19 23:46:11 +0100

    🚑 (path) fourth iteration

--- src/convert_and_merge.py
+++ src/convert_and_merge.py
@@ -4,9 +4,8 @@ from pathlib import Path
 from pdf2image import convert_from_path
 from PyPDF2 import PdfMerger
 
-from src.load_yaml import load_options
-
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+from src.load_yaml import load_options
 
 
 def pdf_to_jpg(path_pdf: str, path_jpg: str) -> None:
